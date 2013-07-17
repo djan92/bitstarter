ae8305f7823f3cae83566f60ec2e01074ea2de72
Danny An <djan92@gmail.com>
2013-07-17 21:15:15 +0000

    Edited web.js to buffer index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var buffer = fs.readFile(index.html);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf8', 0, buffer.length));
 });
 
 var port = process.env.PORT || 5000;
